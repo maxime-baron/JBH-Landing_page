73cc284870dab9206ecdcd46046de3c61ee04e67
Maxime Baron <maxime.baron2002@gmail.com>
2025-04-09 22:16:17 +0200

    :bug: Fix play button redirect

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -74,7 +74,7 @@ export default async function Navbar() {
                         </h1>
 
                     </div>
-                    <Link href="/karakaku">
+                    <Link href={`${websiteUrl}/karakaku`}>
                         <button className={styles.playButton}>
                             <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path fill-rule="evenodd" clip-rule="evenodd" d="M7.96722 0.925476C7.64992 1.24278 7.64992 1.75722 7.96722 2.07452L13.0802 7.1875H1.50008C1.05135 7.1875 0.687581 7.55127 0.687581 8C0.687581 8.44873 1.05135 8.8125 1.50008 8.8125H13.0802L7.96722 13.9255C7.64992 14.2428 7.64992 14.7572 7.96722 15.0745C8.28452 15.3918 8.79897 15.3918 9.11627 15.0745L15.6163 8.57452C15.9336 8.25722 15.9336 7.74278 15.6163 7.42548L9.11627 0.925476C8.79897 0.608175 8.28452 0.608175 7.96722 0.925476Z" fill="#FFF6F1" />
